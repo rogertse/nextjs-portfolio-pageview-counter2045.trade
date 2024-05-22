294cb7640b58ba670a391ca73a219fefab9be2f1
rogertse <39270736+rogertse@users.noreply.github.com>
2024-05-22 18:47:21 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import Particles from "./components/particles";
 
 const navigation = [
-	{ name: "chat", href:"https://chat.2045.trade/" target="_blank" },
+	{ name: "Chat", href:"https://chat.2045.trade/" target="_blank" },
 	{ name: "Join", href: "/contact" },
 ];
 
